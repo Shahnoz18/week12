3bcfbdedd10c9efbad45a9a437cb6b421283dc22
Shahnoz18 <shob181296@mail.ru>
2023-04-10 00:35:33 +0300

    twenty

---
+++ assets/script/index.js
@@ -0,0 +1,24 @@
+const nameElement = document.getElementById("fio");
+const fioResultElement = document.getElementById("fio-result");
+const date = document.getElementById("date");
+const commentsElement = document.getElementById("comments");
+const commentsResultElement = document.getElementById("comments-result");
+const imageElement = document.getElementById("imageResult");
+const button = document.getElementById("button");
+
+let commentChecked = () => {
+    let checkSpam = comments.replace(/viagra/gi, "***").replace(/xxx/gi, "***");
+    console.log(checkSpam);
+}
+
+button.addEventListener('click', () => {
+    const name = nameElement.value;
+    // console.log(name); -проверка в консоли 
+    fioResultElement.value = name;
+    const comments = commentsElement.value;
+    commentsResultElement.value = comments;
+
+})
+
+// let nameElementChaked = nameElement.trim(); //проверка на пробелы
+
